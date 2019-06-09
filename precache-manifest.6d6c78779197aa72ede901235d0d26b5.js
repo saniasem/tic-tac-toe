fe1257a1414653291034b4c90e3565f5d756ac58
Oleksandr Semeniuk <olexandr.semenyuk@hotmail.com>
2019-06-09 10:27:17 +0300

    Updates

---
+++ precache-manifest.6d6c78779197aa72ede901235d0d26b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4feca2d7b17125644a0bd9ff209774",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ae589541ed7421e06ea4",
+    "url": "/tic-tac-toe/static/css/main.ea79d41a.chunk.css"
+  },
+  {
+    "revision": "1439f38f9ece51aaf058",
+    "url": "/tic-tac-toe/static/js/2.fde396be.chunk.js"
+  },
+  {
+    "revision": "ae589541ed7421e06ea4",
+    "url": "/tic-tac-toe/static/js/main.2ea1fb73.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
